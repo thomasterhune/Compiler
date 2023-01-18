3a71e609227b4053b7ed8de3fad5e7698241a206
Thomas Terhune <thomasterh99@gmail.com>
2023-01-18 14:48:30 -0500

    Changes

--- main.c
+++ main.c
@@ -8,75 +8,87 @@ void addExtension(char* filename, char* extension) {
 }
 
 bool hasExtension(char* filename){
-	bool Extension_Found = false;
+	bool extensionFound = false;
 	int i = 0;
 	
 	for(i; i < strlen(filename) && !Extension_Found; i++){
 		if(filename[i] == '.'){
-			Extension_Found = true;
+			extensionFound = true;
 		}
 	}
 	
-	return Extension_Found;
+	return extensionFound;
 }
 
-bool promptFileName(char* Input_File_Name) {
-	bool NotEntered = false;
+bool promptFileName(char* inputFileName) {
+	bool notEntered = false;
 	printf("Enter the input file name: ");
-	scanf("%s", Input_File_Name);
+	scanf("%s", inputFileName);
+	if (inputFileName[0] == '\0'){
+		notEntered = true;
+	}
 	
-	return NotEntered;
+	
+	return notEntered;
 }
 
-bool openInputFile(char* Input_File_Name, FILE* Input_File) {
+bool openInputFile(char* inputFileName, FILE* inputFile) {
 	bool terminate = false;
 	//If no extension add .IN
-	if(!hasExtension(Input_File_Name)) {
-		addExtension(Input_File_Name, ".IN");
+	if(!hasExtension(inputFileName)) {
+		addExtension(inputFileName, ".IN");
 	}
 	
 	/*if does not exist*/
-	Input_File = fopen(Input_File_Name, "r");
-	if(Input_File == NULL){
+	inputFile = fopen(inputFileName, "r");
+	if(inputFile == NULL){
 		//promptFileName();
 	}
+	
+	
+	return terminate;
 }
 
-void openOutputFile(char* Output_File_Name, FILE* Output_File) {
+void openOutputFile(char* outputFileName, FILE* outputFile) {
 	//If no extension add .OUT
-	if(!hasExtension(Output_File_Name)) {
-		addExtension(Output_File_Name, ".OUT");
+	if(!hasExtension(outputFileName)) {
+		addExtension(outputFileName, ".OUT");
 	}
 		
-	Output_File = fopen(Output_File_Name, "w");
+	outputFile = fopen(outputFileName, "w");
 	//if exists choose to overwrite, enter new output file name, or terminate
 }
 
 int main(int argc, char *argv[]) {
 	
-	char Input_File_Name[100];
-	char Output_File_Name[100];
-	FILE * Input_File, Output_File;
+	char inputFileName[100];
+	char outputFileName[100];
+	FILE * inputFile, outputFile;
 	
 	/*Command Line Parameters*/
 	if(argc == 3) {
 		//Copy string from command line args
-		strcpy(Input_File_Name, argv[1]);
+		strcpy(inputFileName, argv[1]);
 		
-		if(openInputFile(Input_File_Name, Input_File)){
+		if(openInputFile(inputFileName, inputFile)) {
 			//Copy string from command line args
-			strcpy(Input_File_Name, argv[1]);
+			strcpy(outputFileName, argv[2]);
+			openOutputFile(outputFileName, outputFile);
 		}
-		
-		
-		
 	}else if(argc == 2) {
 		//Print for testing purposeses
 		printf("The input file name is %s\n", argv[1]);
 		//Copy string from command line args
-		strcpy(Input_File_Name, argv[1]);
-	}else {
+		strcpy(inputFileName, argv[1]);
 		
+		openInputFile(inputFileName, inputFile)
+		/*Prompt for output file*/
+		printf("Enter the output file name: ");
+		
+	}else {
+		printf("Enter the input file name: ");
+		
+		printf("Enter the output file name: ");
 	}
 		
 	return 0;
