12256d71cfae3d8645395b8b165e0de4185d5859
klm127 <karlmiller127@gmail.com>
2023-01-19 19:43:13 -0500

    New directory structure

---
+++ src/main.c
@@ -0,0 +1,100 @@
+/**
+  * 
+  * Author: Tom Terhune
+  * Author: Karl Miller
+  * Author: Anthony Stepich
+  * 
+  * program 1 for CSC 460.
+  * 
+  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+void addExtension(char* filename, char* extension) {
+    strcat(filename, extension);
+}
+
+bool hasExtension(char* filename){
+	bool Extension_Found = false;
+	int i = 0;
+	
+	for(i; i < strlen(filename) && !Extension_Found; i++){
+		if(filename[i] == '.'){
+			Extension_Found = true;
+		}
+	}
+	
+	return Extension_Found;
+}
+
+bool promptFileName(char* Input_File_Name) {
+	bool NotEntered = false;
+	printf("Enter the input file name: ");
+	scanf("%s", Input_File_Name);
+	
+	return NotEntered;
+}
+
+bool openInputFile(char* Input_File_Name, FILE* Input_File) {
+	bool terminate = false;
+	/* If no extension add .IN */
+	if(!hasExtension(Input_File_Name)) {
+		addExtension(Input_File_Name, ".IN");
+	}
+	
+	/*if does not exist*/
+	Input_File = fopen(Input_File_Name, "r");
+	if(Input_File == NULL){
+		/*promptFileName();*/
+	}
+}
+
+void openOutputFile(char* Output_File_Name, FILE* Output_File) {
+	/* If no extension add .OUT */
+	if(!hasExtension(Output_File_Name)) {
+		addExtension(Output_File_Name, ".OUT");
+	}
+		
+	Output_File = fopen(Output_File_Name, "w");
+	/* if exists choose to overwrite, enter new output file name, or terminate */
+}
+
+void closeFile(FILE* file){
+	if(file != NULL){
+		fclose(file);
+	}
+}
+
+int main(int argc, char *argv[]) {
+	
+	char Input_File_Name[100];
+	char Output_File_Name[100];
+	FILE * Input_File, Output_File;
+	
+	/*Command Line Parameters*/
+	if(argc == 3) {
+		/* Copy string from command line args */
+		strcpy(Input_File_Name, argv[1]);
+		
+		if(openInputFile(Input_File_Name, Input_File)){
+			/*Copy string from command line args*/
+			strcpy(Input_File_Name, argv[1]);
+		}
+		
+		
+		
+	}else if(argc == 2) {
+		/* Copy string from command line args */
+		strcpy(Input_File_Name, argv[1]);
+	}else {
+		
+	}
+		/* closing files */
+	fclose(Input_File_Name);
+  	fclose(Output_File_Name);
+	
+	return 0;
+}
