1eabe65d0c231f54177808f1c9f99a90d04f0feb
klm127 <karlmiller127@gmail.com>
2023-01-28 22:08:02 -0500

    tests need to compile to root

--- runTests.bat
+++ runTests.bat
@@ -2,6 +2,4 @@
 REM this is handled by VSCode test task but is replicated here for those not using VSCode.
 @Echo on
 gcc -Isrc -Itests/lib src/file_util.c src/compfiles.c tests/lib/CuTest.c tests/lib/std_swapper.c tests/file_util_test.c tests/compfiles_test.c tests/main_test.c -otests -ansi
-cd bin
 tests
-cd ..
