45b07f804781fc6c15ab1595682f055317b8143f
klm127 <karlmiller127@gmail.com>
2023-02-01 18:22:34 -0500

    rebuilt documentation

--- docs/html/search/functions_4.js
+++ docs/html/search/functions_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['getstring_0',['getString',['../file__util_8c.html#ad44c1d6100c8292003fb4c47bee6be6a',1,'getString():&#160;file_util.c'],['../file__util_8h.html#ad44c1d6100c8292003fb4c47bee6be6a',1,'getString():&#160;file_util.c']]]
+  ['generateabsolutepath_0',['generateAbsolutePath',['../file__util_8c.html#ad08cbe46db7fff259ae5c0df627321f2',1,'generateAbsolutePath(const char *filename):&#160;file_util.c'],['../file__util_8h.html#ad08cbe46db7fff259ae5c0df627321f2',1,'generateAbsolutePath(const char *filename):&#160;file_util.c']]],
+  ['getstring_1',['getString',['../file__util_8c.html#ad44c1d6100c8292003fb4c47bee6be6a',1,'getString():&#160;file_util.c'],['../file__util_8h.html#ad44c1d6100c8292003fb4c47bee6be6a',1,'getString():&#160;file_util.c']]]
 ];
